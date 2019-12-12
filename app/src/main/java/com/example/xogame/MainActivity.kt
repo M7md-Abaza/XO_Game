c060abec532790fee1352c917bd30c34ec4b6f57
m7mdAbaza <midoabaza1@gmail.com>
2019-12-12 20:15:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/xogame/MainActivity.kt
@@ -0,0 +1,183 @@
+package com.example.xogame
+
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val buttons: Array<Array<Button?>> =
+        Array(3) { arrayOfNulls<Button>(3) }
+
+    private var player1Turn = true
+
+    private var roundCount = 0
+
+    private var player1Points = 0
+    private var player2Points = 0
+
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // to get the Button position
+        for (i in 0..2) {
+            for (j in 0..2) {
+                /*
+                Notice that the "btn_$i$j" is as same as my buttons id in xml File without numbers
+                as numbers will be add thanks to the next three lines of code
+                */
+                val buttonID = "btn_$i$j"
+                val resID = resources.getIdentifier(buttonID, "id", packageName)
+                buttons[i][j] = findViewById<View>(resID) as Button?
+                buttons[i][j]?.setOnClickListener(this)
+            }
+        }
+
+        // btn_reset for rest Buttons without change players points
+        btn_reset.setOnClickListener {
+            resetBoard()
+            updatePointsText()
+            Toast.makeText(this, "Reset Performed", Toast.LENGTH_SHORT).show()
+            btn_reset.visibility = View.GONE
+
+        }
+        // btn_playAgain for rest Buttons and change players points to zero
+        btn_playAgain.setOnClickListener {
+            resetBoard()
+            player1Points = 0
+            player2Points = 0
+            updatePointsText()
+            Toast.makeText(this, "Reset Performed", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+
+    override fun onClick(v: View) {
+        if ((v as Button).text.toString() != "") {
+            return
+        }
+        if (player1Turn) {
+            v.text = "X"
+        } else {
+            v.text = "O"
+        }
+        roundCount++
+
+        if (checkForWin()) {
+            if (player1Turn) {
+                player1Wins()
+            } else {
+                player2Wins()
+            }
+        } else if (roundCount == 9) {
+            draw()
+        } else {
+            /*
+            this else is for change turn from player one to player two so
+            the game check after checking that no winner and rountCount not equal 9
+            that is mean there in more places"Button" to play
+            */
+            player1Turn = !player1Turn
+        }
+    }
+
+    private fun checkForWin(): Boolean {
+        val field = Array(3) { arrayOfNulls<String>(3) }
+        // Next for_loop using for put buttons[][] array values to field[][] array
+        for (i in 0..2) {
+            for (j in 0..2) {
+                field[i][j] = buttons[i][j]!!.text.toString()
+            }
+        }
+        // Next for_loop using to check the buttons in one row "horizontal" are equal or not
+        for (i in 0..2) {
+            if (field[i][0] == field[i][1] && field[i][0] == field[i][2] && field[i][0] != ""
+            ) {
+                return true
+            }
+        }
+        // Next for_loop using to check the buttons in one column "vertical" are equal or not
+        for (i in 0..2) {
+            if (field[0][i] == field[1][i] && field[0][i] == field[2][i] && field[0][i] != ""
+            ) {
+                return true
+            }
+        }
+        // Next for_loop using to check the buttons from top_left to bottom_right are equal or not
+        if (field[0][0] == field[1][1] && field[0][0] == field[2][2] && field[0][0] != ""
+        ) {
+            return true
+        }
+        // Next last return using to check the buttons from top_right to bottom_left are equal or not
+        if (field[0][2] == field[1][1] && field[0][2] == field[2][0] && field[0][2] != ""
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun player1Wins() {
+        player1Points++
+        Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show()
+        updatePointsText()
+        btn_reset.visibility = View.VISIBLE
+    }
+
+    private fun player2Wins() {
+        player2Points++
+        Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show()
+        updatePointsText()
+        btn_reset.visibility = View.VISIBLE
+    }
+
+    private fun draw() {
+        Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show()
+        resetBoard()
+        btn_reset.visibility = View.VISIBLE
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updatePointsText() {
+        txt_player_1.text = "Player 1: $player1Points"
+        txt_player_2.text = "Player 2: $player2Points"
+    }
+
+    // to clear Buttons screen
+    private fun resetBoard() {
+        for (i in 0..2) {
+            for (j in 0..2) {
+                buttons[i][j]!!.text = ""
+            }
+        }
+        roundCount = 0
+        player1Turn = true
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("roundCount", roundCount)
+        outState.putInt("player1Points", player1Points)
+        outState.putInt("player2Points", player2Points)
+        outState.putBoolean("player1Turn", player1Turn)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        roundCount = savedInstanceState.getInt("roundCount")
+        player1Points = savedInstanceState.getInt("player1Points")
+        player2Points = savedInstanceState.getInt("player2Points")
+        player1Turn = savedInstanceState.getBoolean("player1Turn")
+    }
+}
+
